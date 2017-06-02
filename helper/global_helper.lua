ac45ef7b7538e56c3639dd21b0add9a9060ef19a
Nostrademous <nostrademous@hotmail.com>
2017-06-02 14:18:53 -0400

    added a global helper - to be imported once globally

---
+++ helper/global_helper.lua
@@ -0,0 +1,27 @@
+function GetShop()
+    if (GetTeam() == TEAM_RADIANT) then
+        return SHOP_RADIANT
+    elseif (GetTeam() == TEAM_DIRE) then
+        return SHOP_DIRE
+    end
+end
+
+function GetEnemyTeam()
+    if (GetTeam() == TEAM_RADIANT) then
+        return TEAM_DIRE
+    elseif (GetTeam() == TEAM_DIRE) then
+        return TEAM_RADIANT
+    end
+end
+
+function GetFront(Team, Lane)
+    return GetLaneFrontLocation(Team, Lane, GetLaneFrontAmount(Team, Lane, true))
+end
+
+function Safelane()
+    return ((GetTeam() == TEAM_RADIANT) and LANE_BOT or LANE_TOP)
+end
+
+function Hardlane()
+    return ((GetTeam() == TEAM_RADIANT) and LANE_TOP or LANE_BOT)
+end
