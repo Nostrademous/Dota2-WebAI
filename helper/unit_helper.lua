5082bca3a6e4c97ff52afd46f1f4ddd0f252c3c6
Nostrademous <nostrademous@hotmail.com>
2017-06-05 08:30:49 -0400

    start of unit helper

---
+++ helper/unit_helper.lua
@@ -0,0 +1,21 @@
+local UnitHelper = {}
+
+-- this can be used for unit reaching another unit or projectile reaching a unit
+function UnitHelper:TimeToReachTarget( hUnit, hTarget, fSpeed )
+    local speed = fSpeed
+    if not speed then
+        if not hUnit:IsNull() then
+            speed = hUnit:GetCurrentMovementSpeed()
+        else
+            return VERY_HIGH_INT
+        end
+    end
+    
+    if not hUnit:IsNull() and not hTarget:IsNull() then
+        return GetUnitToUnitDistance( hUnit, hTarget ) / speed
+    end
+    
+    return VERY_HIGH_INT
+end
+
+return UnitHelper
